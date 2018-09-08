c337546fb87d80d4d3178fb971547b02e3bd239d
thecharlesblake <thecharlesblake@live.co.uk>
2018-09-08 23:19:39 +0100

    Add documentation comments

--- pip_app/src/lib.rs
+++ pip_app/src/lib.rs
@@ -1,5 +1,29 @@
-pub fn hello_world() -> String {
-    String::from("Hello World!")
+//!
+//! # Crate test
+//!
+//! Testing
+//!
+//! ## Well...
+//!
+//! This is some more doc text
+//!
+
+///
+/// # An example of a library module
+///
+/// This is just some test doc
+///
+/// ```
+/// assert_eq!(hello_world(), "Xello World!");
+/// ```
+///
+pub mod lib_mod {
+    ///
+    /// fn comment for *hello world* fn
+    ///
+    pub fn hello_world() -> String {
+        String::from("Hello World!")
+    }
 }
 
 #[cfg(test)]
