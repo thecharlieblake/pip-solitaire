6a0bb76b35f5063e35a3514c0af4b090aeb4daad
thecharlesblake <thecharlesblake@live.co.uk>
2018-09-21 23:59:18 +0100

    Initial docs for pip_core

--- pip_core/src/lib.rs
+++ pip_core/src/lib.rs
@@ -1,3 +1,12 @@
+//!
+//! # The core solitaire library for pip
+//!
+//! Contains only features relevant to solitaire, *not* search.
+//!
+
+///
+/// Generates a game from a sorted deck
+///
 pub fn gen_game() -> String {
     unimplemented!()
 }
