83cd2105fe99f3872b29ecff9b25c752b0e4947d
thecharlesblake <thecharlesblake@live.co.uk>
2018-12-22 00:17:00 +0000

    Add search depth limit

--- pip_app/src/lib.rs
+++ pip_app/src/lib.rs
@@ -5,12 +5,13 @@ extern crate petgraph;
 use pip_core::{utils, gen_game, Options};
 use std::fmt::Debug;
 use petgraph::{Graph, Direction, graph::NodeIndex};
+use std::{hash::Hash, collections::HashSet};
 
 pub fn run(ops: Options) {
     println!("{}", utils::yaml::to_pretty_string(&gen_game(ops)))
 }
 
-pub trait Solvable: Sized + Debug + PartialEq + Eq + Clone {
+pub trait Solvable: Sized + Debug + PartialEq + Eq + Clone + Hash {
     fn next(&self) -> Vec<Self>;
     fn is_solved(&self) -> bool;
 }
@@ -20,7 +21,10 @@ struct SearchTree<S: Solvable> {
     search_graph: Graph<S, ()>,
     root_node: NodeIndex<u32>,
     current_node: NodeIndex<u32>,
+    state_limit: Option<u64>,
+    states_visited: u64,
     search_state: SearchState,
+    //transposition_table: HashSet<S>,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -34,14 +38,23 @@ impl<S: Solvable> SearchTree<S> {
     pub fn new(s: S) -> Self {
         let mut search_graph = Graph::<S, ()>::new();
         let root_node = search_graph.add_node(s);
-        let current_node = root_node;
-        let search_state = SearchState::InProgress;
 
         // Sets up the search invariant (children expanded)
-        let mut search_tree = Self { search_graph, root_node, current_node, search_state };
-        search_tree.add_next(current_node);
+        let mut search_tree = Self {
+            search_graph,
+            root_node,
+            current_node: root_node,
+            state_limit: None,
+            states_visited: 0,
+            search_state: SearchState::InProgress,
+        };
+        search_tree.add_next(root_node);
 
         search_tree
+    }
+
+    pub fn set_depth_limit(&mut self, limit: u64) {
+        self.state_limit = Some(limit)
     }
 
     pub fn dfs(&mut self) -> Option<Vec<S>> {
@@ -49,14 +62,19 @@ impl<S: Solvable> SearchTree<S> {
 
         self.search_state = self.check_solution();
 
-        // Invariant: current_node is a valid node in search_graph, with its unsearched children expanded
-        while self.search_state == InProgress {
-            println!("{:#?}", self);
+        // Invariant: current_node is a valid node in search_graph, with its unsearched children expanded,
+        // and with a state that beed added to the transposition table
+        while self.continue_search() {
+            //println!("{:#?}", self);
             match self.next_child() {
                 Some(n) => {
-                    self.add_next(n);
-                    self.assign_current(n);
-                    self.search_state = self.check_solution();
+                    if self.cache(n) {
+                        self.add_next(n);
+                        self.assign_current(n);
+                        self.search_state = self.check_solution();
+                    } else {
+                        self.remove(n);
+                    }
                 },
                 None => {
                     let old_node_ref = self.current_node.clone();
@@ -76,6 +94,11 @@ impl<S: Solvable> SearchTree<S> {
         }
     }
 
+    fn continue_search(&self) -> bool {
+        self.search_state == SearchState::InProgress
+            && self.state_limit.map_or(true, |l| l > self.states_visited)
+    }
+
     fn add_next(&mut self, node: NodeIndex) {
         for child_state in self.search_graph.node_weight(node).unwrap().next() {
             let child_node = self.search_graph.add_node(child_state);
@@ -83,14 +106,20 @@ impl<S: Solvable> SearchTree<S> {
         }
     }
 
-    fn check_solution(&self) -> SearchState {
+    fn check_solution(&mut self) -> SearchState {
         use SearchState::*;
+
+        self.states_visited += 1;
 
         if self.search_graph.node_weight(self.current_node).unwrap().is_solved() {
             Solved
         } else {
             InProgress
         }
+    }
+
+    fn cache(&mut self, n: NodeIndex) -> bool {
+        true//self.transposition_table.insert(self.search_graph.node_weight(n).unwrap())
     }
 
     fn next_child(&self) -> Option<NodeIndex> {
@@ -198,4 +227,60 @@ mod tests {
             SearchTree::new(7).dfs(),
         );
     }
+
+    #[test]
+    fn one_state_visited() {
+        let mut s = SearchTree::new(72);
+        s.dfs();
+        assert_eq!(
+            1,
+            s.states_visited,
+        );
+    }
+
+    #[test]
+    fn two_states_visited() {
+        let mut s = SearchTree::new(36);
+        s.dfs();
+        assert_eq!(
+            2,
+            s.states_visited,
+        );
+    }
+
+    #[test]
+    fn ten_states_visited() {
+        let mut s = SearchTree::new(3);
+        s.dfs();
+        assert_eq!(
+            8,
+            s.states_visited,
+        );
+    }
+
+    #[test]
+    fn search_state_limit_cutoff() {
+        let mut s = SearchTree::new(3);
+        s.set_depth_limit(7);
+        s.dfs();
+        assert_eq!(
+            SearchState::InProgress,
+            s.search_state,
+
+        );
+    }
+
+    #[test]
+    fn search_state_limit_not_cutoff() {
+        let mut s = SearchTree::new(3);
+        s.set_depth_limit(8);
+        s.dfs();
+        assert_eq!(
+            SearchState::Solved,
+            s.search_state,
+
+        );
+    }
+
+    // TODO: cache loop test, cache implementation
 }
