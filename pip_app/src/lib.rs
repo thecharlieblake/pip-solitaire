c349dcc676c90a5a9c98e24ed682bac0a77a5c49
thecharlesblake <thecharlesblake@live.co.uk>
2018-12-22 20:22:04 +0000

    Remove cache and replace graph with graph map

--- pip_app/src/lib.rs
+++ pip_app/src/lib.rs
@@ -4,27 +4,26 @@ extern crate petgraph;
 
 use pip_core::{utils, gen_game, Options};
 use std::fmt::Debug;
-use petgraph::{Graph, Direction, graph::NodeIndex};
-use std::{hash::Hash, collections::HashSet};
+use petgraph::{graphmap::DiGraphMap, Direction};
+use std::{hash::Hash};
 
 pub fn run(ops: Options) {
     println!("{}", utils::yaml::to_pretty_string(&gen_game(ops)))
 }
 
-pub trait Solvable: Sized + Debug + PartialEq + Eq + Clone + Hash {
+pub trait Solvable: PartialEq + Eq + Ord + Copy + Hash + Debug {
     fn next(&self) -> Vec<Self>;
     fn is_solved(&self) -> bool;
 }
 
 #[derive(Debug)]
 struct SearchTree<S: Solvable> {
-    search_graph: Graph<S, ()>,
-    root_node: NodeIndex<u32>,
-    current_node: NodeIndex<u32>,
+    search_graph: DiGraphMap<S, ()>,
+    initial: S,
+    current: S,
     state_limit: Option<u64>,
     states_visited: u64,
     search_state: SearchState,
-    //transposition_table: HashSet<S>,
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -36,19 +35,19 @@ enum SearchState {
 
 impl<S: Solvable> SearchTree<S> {
     pub fn new(s: S) -> Self {
-        let mut search_graph = Graph::<S, ()>::new();
-        let root_node = search_graph.add_node(s);
+        let mut search_graph = DiGraphMap::new();
+        search_graph.add_node(s);
 
         // Sets up the search invariant (children expanded)
         let mut search_tree = Self {
             search_graph,
-            root_node,
-            current_node: root_node,
+            initial: s,
+            current: s,
             state_limit: None,
             states_visited: 0,
             search_state: SearchState::InProgress,
         };
-        search_tree.add_next(root_node);
+        search_tree.add_next(s);
 
         search_tree
     }
@@ -62,27 +61,24 @@ impl<S: Solvable> SearchTree<S> {
 
         self.search_state = self.check_solution();
 
-        // Invariant: current_node is a valid node in search_graph, with its unsearched children expanded,
-        // and with a state that beed added to the transposition table
+        // Invariant: current_node is a valid node in search_graph, with its unsearched children expanded
         while self.continue_search() {
-            //println!("{:#?}", self);
+            println!("{:#?}", self);
             match self.next_child() {
-                Some(n) => {
-                    if self.cache(n) {
-                        self.add_next(n);
-                        self.assign_current(n);
-                        self.search_state = self.check_solution();
-                    } else {
-                        self.remove(n);
-                    }
+                Some(s) => {
+                    self.add_next(s);
+                    self.current = s;
+                    self.search_state = self.check_solution();
                 },
                 None => {
-                    let old_node_ref = self.current_node.clone();
+                    let old_current = self.current;
                     match self.parent() {
-                        Some(n) => self.assign_current(n),
+                        Some(parent) => {
+                            self.current = parent;
+                            self.remove(old_current, parent);
+                        },
                         None => self.search_state = NoSolution,
                     }
-                    self.remove(old_node_ref)
                 },
             }
         }
@@ -99,10 +95,12 @@ impl<S: Solvable> SearchTree<S> {
             && self.state_limit.map_or(true, |l| l > self.states_visited)
     }
 
-    fn add_next(&mut self, node: NodeIndex) {
-        for child_state in self.search_graph.node_weight(node).unwrap().next() {
-            let child_node = self.search_graph.add_node(child_state);
-            self.search_graph.add_edge(node, child_node, ());
+    fn add_next(&mut self, s: S) {
+        for child in s.next() {
+            if !self.search_graph.contains_node(child) {
+                self.search_graph.add_node(child);
+                self.search_graph.add_edge(s, child, ());
+            }
         }
     }
 
@@ -111,44 +109,36 @@ impl<S: Solvable> SearchTree<S> {
 
         self.states_visited += 1;
 
-        if self.search_graph.node_weight(self.current_node).unwrap().is_solved() {
+        if self.current.is_solved() {
             Solved
         } else {
             InProgress
         }
     }
 
-    fn cache(&mut self, n: NodeIndex) -> bool {
-        true//self.transposition_table.insert(self.search_graph.node_weight(n).unwrap())
+    fn next_child(&self) -> Option<S> {
+        self.search_graph.neighbors_directed(self.current, Direction::Outgoing).into_iter().next()
     }
 
-    fn next_child(&self) -> Option<NodeIndex> {
-        self.search_graph.neighbors_directed(self.current_node, Direction::Outgoing).into_iter().next()
+    fn parent(&self) -> Option<S> {
+        self.search_graph.neighbors_directed(self.current, Direction::Incoming).into_iter().next()
     }
 
-    fn parent(&self) -> Option<NodeIndex> {
-        self.search_graph.neighbors_directed(self.current_node, Direction::Incoming).into_iter().next()
-    }
-
-    fn assign_current(&mut self, node: NodeIndex) {
-        self.current_node = node.clone();
-    }
-
-    fn remove(&mut self, node: NodeIndex) {
-        self.search_graph.remove_node(node);
+    fn remove(&mut self, s: S, parent: S) {
+        self.search_graph.remove_edge(parent, s);
+        self.search_graph.remove_node(s);
     }
 
     fn search_path(&self) -> Vec<S> {
         let mut states = Vec::new();
-        let mut n = self.root_node;
+        let mut s = self.initial;
 
         loop {
-            states.push(self.search_graph.node_weight(n).unwrap().clone());
-            if n == self.current_node {
+            states.push(s);
+            if s == self.current {
                 break
             }
-            n = self.search_graph.neighbors_directed(n, Direction::Outgoing).into_iter()
-                .next().unwrap().clone()
+            s = self.search_graph.neighbors_directed(s, Direction::Outgoing).into_iter().next().unwrap()
         }
 
         states
@@ -162,7 +152,7 @@ mod tests {
     impl Solvable for u64 {
         fn next(&self) -> Vec<Self> {
             if *self < 72 {
-                vec![self * 3, self * 2]
+                vec![self * 2, self * 3, self * 1]
             } else {
                 vec![]
             }
@@ -172,115 +162,100 @@ mod tests {
         }
     }
 
+    fn setup_tree(init: u64) -> SearchTree<u64> {
+        let mut t = SearchTree::new(init);
+        t.set_depth_limit(10000);
+        t
+    }
+
     #[test]
     fn dfs_num_test_1() {
-        assert_eq!(
-            Some(vec![1, 2, 4, 8, 24, 72]),
-            SearchTree::new(1).dfs(),
-        );
+        let mut t = setup_tree(1);
+
+        assert_eq!(Some(vec![1, 2, 4, 8, 24, 72]), t.dfs());
     }
 
     #[test]
     fn dfs_num_test_2() {
-        assert_eq!(
-            Some(vec![2, 4, 8, 24, 72]),
-            SearchTree::new(2).dfs(),
-        );
+        let mut t = setup_tree(2);
+
+        assert_eq!(Some(vec![2, 4, 8, 24, 72]), t.dfs());
     }
 
     #[test]
     fn dfs_num_test_3() {
-        assert_eq!(
-            Some(vec![3, 6, 12, 24, 72]),
-            SearchTree::new(3).dfs(),
-        );
+        let mut t = setup_tree(3);
+
+        assert_eq!(Some(vec![3, 6, 12, 24, 72]), t.dfs());
     }
 
     #[test]
     fn dfs_num_test_4() {
-        assert_eq!(
-            Some(vec![4, 8, 24, 72]),
-            SearchTree::new(4).dfs(),
-        );
+        let mut t = setup_tree(4);
+
+        assert_eq!(Some(vec![4, 8, 24, 72]), t.dfs());
     }
 
     #[test]
     fn dfs_num_test_5() {
-        assert_eq!(
-            None,
-            SearchTree::new(5).dfs(),
-        );
+        let mut t = setup_tree(5);
+
+        assert_eq!(None, t.dfs());
     }
 
     #[test]
     fn dfs_num_test_6() {
-        assert_eq!(
-            Some(vec![6, 12, 24, 72]),
-            SearchTree::new(6).dfs(),
-        );
+        let mut t = setup_tree(6);
+
+        assert_eq!(Some(vec![6, 12, 24, 72]), t.dfs());
     }
 
     #[test]
     fn dfs_num_test_7() {
-        assert_eq!(
-            None,
-            SearchTree::new(7).dfs(),
-        );
+        let mut t = setup_tree(7);
+
+        assert_eq!(None, t.dfs());
     }
 
     #[test]
     fn one_state_visited() {
-        let mut s = SearchTree::new(72);
-        s.dfs();
-        assert_eq!(
-            1,
-            s.states_visited,
-        );
+        let mut t = setup_tree(72);
+        t.dfs();
+
+        assert_eq!(1, t.states_visited);
     }
 
     #[test]
     fn two_states_visited() {
-        let mut s = SearchTree::new(36);
-        s.dfs();
-        assert_eq!(
-            2,
-            s.states_visited,
-        );
+        let mut t = setup_tree(36);
+        t.dfs();
+
+        assert_eq!(2, t.states_visited);
     }
 
     #[test]
-    fn ten_states_visited() {
-        let mut s = SearchTree::new(3);
-        s.dfs();
-        assert_eq!(
-            8,
-            s.states_visited,
-        );
+    fn eight_states_visited() {
+        let mut t = setup_tree(3);
+        t.dfs();
+
+        assert_eq!(8, t.states_visited);
     }
 
     #[test]
     fn search_state_limit_cutoff() {
-        let mut s = SearchTree::new(3);
-        s.set_depth_limit(7);
-        s.dfs();
-        assert_eq!(
-            SearchState::InProgress,
-            s.search_state,
+        let mut t = SearchTree::new(3);
+        t.set_depth_limit(7);
+        t.dfs();
 
-        );
+        assert_eq!(SearchState::InProgress, t.search_state);
     }
 
     #[test]
     fn search_state_limit_not_cutoff() {
-        let mut s = SearchTree::new(3);
-        s.set_depth_limit(8);
-        s.dfs();
-        assert_eq!(
-            SearchState::Solved,
-            s.search_state,
+        let mut t = SearchTree::new(3);
+        t.set_depth_limit(8);
+        t.dfs();
 
-        );
+        assert_eq!(SearchState::Solved, t.search_state);
     }
-
-    // TODO: cache loop test, cache implementation
 }
